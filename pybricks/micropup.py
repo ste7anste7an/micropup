f96f53a50003e20e0b007f489d7f4b81ec6c244d
Stefan Op de Beek <ste7an@gmail.com>
2025-04-21 14:54:24 +0200

    first working version of micropup library

--- pybricks/micropup.py
+++ pybricks/micropup.py
@@ -1,10 +1,12 @@
+from pybricks.parameters import Port, Color
 from pybricks.iodevices import PUPDevice
 from micropython import const
 MODE = const(0)
 NAME = const(1)
 TO_HUB = const(2)
 FROM_HUB = const(3)
-DATA_TYPE = const(4)
+#DATA_TYPE = const(4)
+#DATA_TYPES = ['b':0, 'w':1, 'i':2]
 
 class MicroPUP:
     def __init__(self, port):
@@ -22,11 +24,11 @@ class MicroPUP:
         command_name: str,
         to_hub_cnt: int = 0,
         from_hub_cnt: int = 0,
-        data_type: int = 0,
+        #data_type: int = 0,
     ):
         
         self.commands[command_name] = [
-                self.nr_commands, to_hub_cnt, from_hub_cnt, data_type
+                self.nr_commands, to_hub_cnt, from_hub_cnt  #, data_type
             ]
         self.nr_commands+=1
 
@@ -35,11 +37,33 @@ class MicroPUP:
         Call a remote function on the sensor side with the mode_name you defined on both sides.
 
         """
-
-        mode, to_hub_cnt, from_hub_cnt, data_type = self.commands[command_name]
+        #mode, to_hub_cnt, from_hub_cnt, data_type = self.commands[command_name]
+        mode, to_hub_cnt, from_hub_cnt = self.commands[command_name]
         if from_hub_cnt>0 and len(argv)>0:
             self.pup_device.write(mode,argv)
             
         if to_hub_cnt > 0:
             response =  self.pup_device.read(mode)
+            if len(response)==1:
+                response = response[0]
             return response
+p=None
+
+def pup_init(port):
+    global p
+    p=MicroPUP(eval('Port.'+port))
+
+def pup_add_command(command_name: str,
+        to_hub_cnt: int = 0,
+        from_hub_cnt: int = 0,
+        # data_type: int = 0,
+    ):
+    global p
+    p.add_command(command_name, to_hub_cnt, from_hub_cnt)# , data_type)
+
+
+def pup_call(*argv):
+    global p
+    command_name = argv[0]
+    response = p.call(command_name,*argv[1:])
+    return response

080f66cbb269a127774ee38d30eaf60d84cb4d03
Stefan Op de Beek <ste7an@gmail.com>
2025-04-26 22:05:06 +0200

    micropup return only to_hub_cnt variables

--- pybricks/micropup.py
+++ pybricks/micropup.py
@@ -5,8 +5,6 @@ MODE = const(0)
 NAME = const(1)
 TO_HUB = const(2)
 FROM_HUB = const(3)
-#DATA_TYPE = const(4)
-#DATA_TYPES = ['b':0, 'w':1, 'i':2]
 
 class MicroPUP:
     def __init__(self, port):
@@ -24,7 +22,7 @@ class MicroPUP:
         command_name: str,
         to_hub_cnt: int = 0,
         from_hub_cnt: int = 0,
-        #data_type: int = 0,
+
     ):
         
         self.commands[command_name] = [
@@ -37,13 +35,13 @@ class MicroPUP:
         Call a remote function on the sensor side with the mode_name you defined on both sides.
 
         """
-        #mode, to_hub_cnt, from_hub_cnt, data_type = self.commands[command_name]
         mode, to_hub_cnt, from_hub_cnt = self.commands[command_name]
         if from_hub_cnt>0 and len(argv)>0:
             self.pup_device.write(mode,argv)
             
         if to_hub_cnt > 0:
             response =  self.pup_device.read(mode)
+            response = response[:to_hub_cnt]
             if len(response)==1:
                 response = response[0]
             return response

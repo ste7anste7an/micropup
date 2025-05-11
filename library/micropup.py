ccd61f67eeb8fa5370dc3155c11ce06c5415e6fc
Stefan Op de Beek <ste7an@gmail.com>
2025-05-11 11:38:04 +0200

    some changes in library

--- library/micropup.py
+++ library/micropup.py
@@ -5,6 +5,8 @@ MODE = const(0)
 NAME = const(1)
 TO_HUB = const(2)
 FROM_HUB = const(3)
+#DATA_TYPE = const(4)
+#DATA_TYPES = ['b':0, 'w':1, 'i':2]
 
 class MicroPUP:
     def __init__(self, port):
@@ -20,13 +22,13 @@ class MicroPUP:
     def add_command(
         self,
         command_name: str,
-        to_hub_cnt: int = 0,
-        from_hub_cnt: int = 0,
-
+        to_hub: int = 0,
+        from_hub: int = 0,
+        #data_type: int = 0,
     ):
         
         self.commands[command_name] = [
-                self.nr_commands, to_hub_cnt, from_hub_cnt  #, data_type
+                self.nr_commands, to_hub, from_hub  #, data_type
             ]
         self.nr_commands+=1
 
@@ -35,13 +37,16 @@ class MicroPUP:
         Call a remote function on the sensor side with the mode_name you defined on both sides.
 
         """
-        mode, to_hub_cnt, from_hub_cnt = self.commands[command_name]
-        if from_hub_cnt>0 and len(argv)>0:
-            self.pup_device.write(mode,argv)
+        #mode, to_hub_cnt, from_hub_cnt, data_type = self.commands[command_name]
+        mode, to_hub, from_hub = self.commands[command_name]
+        if from_hub>0 and len(argv)>0:
+            # convert to int (in case values are float)
+            arg_int = [int(arg) for arg in argv]
+            self.pup_device.write(mode,arg_int)
             
-        if to_hub_cnt > 0:
+        if to_hub > 0:
             response =  self.pup_device.read(mode)
-            response = response[:to_hub_cnt]
+            response = response[:to_hub]
             if len(response)==1:
                 response = response[0]
             return response
@@ -52,12 +57,12 @@ def pup_init(port):
     p=MicroPUP(eval('Port.'+port))
 
 def pup_add_command(command_name: str,
-        to_hub_cnt: int = 0,
-        from_hub_cnt: int = 0,
+        to_hub: int = 0,
+        from_hub: int = 0,
         # data_type: int = 0,
     ):
     global p
-    p.add_command(command_name, to_hub_cnt, from_hub_cnt)# , data_type)
+    p.add_command(command_name, to_hub, from_hub)# , data_type)
 
 
 def pup_call(*argv):
